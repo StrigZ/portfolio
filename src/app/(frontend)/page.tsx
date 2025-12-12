b728e5c974a457d9f79e523df9fb2d1238cc522d
StrigZ <mirgasan200@gmail.com>
2025-12-12 18:32:19 +0300

    Init next-themes

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,5 @@
+import { ThemeButton } from "./_components/ThemeButton";
+
 export default async function HomePage() {
-	return <h1 className="px-2 text-red-50">HELLO</h1>;
+	return <ThemeButton />;
 }
