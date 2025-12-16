a48d672d56d8c8a000cf83998e14a3f23def072d
StrigZ <mirgasan200@gmail.com>
2025-12-16 20:35:57 +0300

    Adjust carousel styles

--- src/app/(frontend)/_components/ProjectsCarouselItem.tsx
+++ src/app/(frontend)/_components/ProjectsCarouselItem.tsx
@@ -6,72 +6,95 @@ import { buttonVariants } from "./ui/button";
 
 type Props = { project: Project; className?: string };
 export default function ProjectsCarouselItem({ project, className }: Props) {
+	if (typeof project.featuredImage === "number") {
+		throw new Error("ProjectsCarouselItem: Media is not accessible!");
+	}
+
 	return (
 		<article
-			className={cn("flex h-full w-full flex-col gap-4 px-4 py-2", className)}
+			className={cn(
+				"flex h-[500px] w-full select-none flex-col overflow-hidden rounded-lg shadow-white/50 shadow-xs sm:h-[600px] md:h-full",
+				className,
+			)}
 		>
-			<h3 className="text-start text-2xl">{project.title}</h3>
-			<p className="overflow-y-auto text-start text-sm">
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ratione
-				nostrum, tempore voluptatibus mollitia ipsa nam? Non at aut, laboriosam
-				illo tempore nesciunt aspernatur sapiente qui temporibus ex? Magnam
-				dolorem numquam rem modi, qui et nam laboriosam tenetur, nemo
-				perferendis quam neque esse nihil ullam, reiciendis tempore id molestias
-				harum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-				praesentium cumque harum tempora deleniti assumenda quibusdam natus
-				autem unde, similique eos distinctio et provident, nemo soluta incidunt,
-				numquam perferendis nam facilis nulla dignissimos consequatur eius. Nam
-				deleniti, laudantium vero possimus saepe culpa ipsum illo fuga
-				temporibus voluptatum quis sunt quo.
-			</p>
+			<figure className="relative h-1/2 w-full shrink-0 md:hidden">
+				<Image
+					alt={project.featuredImage.alt}
+					className="shrink-0 object-cover object-top sm:object-fill sm:object-center"
+					fill
+					src={project.featuredImage.url ?? project.featuredImage.blurDataUrl}
+				/>
+				<div className="absolute bottom-0 left-0 w-full bg-black/25 p-4 py-2">
+					<h3 className="w-full text-start text-2xl">{project.title}</h3>
+				</div>
+			</figure>
+			<div className="flex h-1/2 flex-col gap-4 p-4 md:h-full">
+				<h3 className="hidden w-full text-start text-2xl md:inline">
+					{project.title}
+				</h3>
+				<p className="mb-auto max-h-full overflow-y-auto text-start text-sm">
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae
+					ratione nostrum, tempore voluptatibus mollitia ipsa nam? Non at aut,
+					laboriosam illo tempore nesciunt aspernatur sapiente qui temporibus
+					ex? Magnam dolorem numquam rem modi, qui et nam laboriosam tenetur,
+					nemoLorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem
+					ipsum, dolor sit amet consectetur adipisicing elit. Vitae ratione
+					nostrum, tempore voluptatibus mollitia ipsa nam? Non at aut,
+					laboriosam illo tempore nesciunt aspernatur sapiente qui temporibus
+					ex? Magnam dolorem numquam rem modi, qui et nam laboriosam tenetur,
+					nemoLorem ipsum, dolor sit amet consectetur adipisicing elit.
+				</p>
 
-			<ul className="flex items-center justify-center gap-2">
-				{project.technologies?.map((tech) => {
-					if (typeof tech.icon === "number") {
-						throw new Error("ProjectsCarouselItem: Media is not accessible!");
-					}
+				<ul className="flex items-center justify-center gap-2">
+					{project.technologies?.map((tech) => {
+						if (typeof tech.icon === "number") {
+							throw new Error("ProjectsCarouselItem: Media is not accessible!");
+						}
 
-					return (
-						<li
-							className="flex flex-col items-center justify-center gap-1 border border-rose-200 p-1"
-							key={tech.id}
-						>
-							<figure className="relative h-8 w-8">
-								<Image
-									alt={tech.icon.alt}
-									fill
-									src={tech.icon.url ?? tech.icon.blurDataUrl}
-								/>
-							</figure>
-							{/* Tooltip? Link? */}
-							{/* <p>{tech.name}</p> */}
-						</li>
-					);
-				})}
-			</ul>
-
-			<div className="mt-auto flex w-full items-center justify-between gap-2">
-				<a
-					className={buttonVariants({
-						className: "flex flex-1 items-center justify-center gap-2",
+						return (
+							<li
+								className="flex flex-col items-center justify-center gap-1 border border-rose-200 p-1"
+								key={tech.id}
+							>
+								<figure className="relative h-8 w-8">
+									<Image
+										alt={tech.icon.alt}
+										fill
+										src={tech.icon.url ?? tech.icon.blurDataUrl}
+									/>
+								</figure>
+								{/* Tooltip? Link? */}
+								{/* <p>{tech.name}</p> */}
+							</li>
+						);
 					})}
-					href={project.demo_url}
-					rel="noreferrer noopener"
-					target="_blank"
-				>
-					DEMO <Link size={16} />
-				</a>
-				<a
-					className={buttonVariants({
-						variant: "outline",
-						className: "flex flex-1 items-center justify-center gap-2",
-					})}
-					href={project.source_url}
-					rel="noreferrer noopener"
-					target="_blank"
-				>
-					Source <Link size={16} />
-				</a>
+				</ul>
+
+				<div className="flex w-full justify-between gap-2">
+					<a
+						className={buttonVariants({
+							className:
+								"flex flex-1 items-center justify-center gap-2 hover:underline",
+						})}
+						href={project.demo_url}
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						Live <Link size={16} />
+					</a>
+					<a
+						className={buttonVariants({
+							variant: "outline",
+							className:
+								"flex flex-1 items-center justify-center gap-2 hover:underline",
+						})}
+						href={project.source_url}
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						Source <Link size={16} />
+					</a>
+				</div>
 			</div>
 		</article>
 	);
--- src/app/(frontend)/_components/SimpleCarousel.tsx
+++ src/app/(frontend)/_components/SimpleCarousel.tsx
@@ -1,14 +1,17 @@
-import Autoplay from "embla-carousel-autoplay";
+"use client";
 
+import Autoplay from "embla-carousel-autoplay";
+import { MoveLeft, MoveRight } from "lucide-react";
+import { useEffect, useState } from "react";
 import { cn } from "~/lib/utils";
 import type { Project } from "~/payload-types";
 import ProjectsCarouselItem from "./ProjectsCarouselItem";
+import { Button } from "./ui/button";
 import {
 	Carousel,
+	type CarouselApi,
 	CarouselContent,
-	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
+	CarouselItem as CarouselItemWrapper,
 } from "./ui/carousel";
 
 type Props = {
@@ -16,9 +19,17 @@ type Props = {
 	className?: string;
 };
 export function SimpleCarousel({ projects, className }: Props) {
+	const [api, setApi] = useState<CarouselApi>();
+
+	useEffect(() => {
+		if (!api) {
+			return;
+		}
+	}, [api]);
+
 	return (
 		<Carousel
-			className={cn("w-full", className)}
+			className={cn("flex w-full flex-col gap-4", className)}
 			opts={{
 				align: "start",
 				loop: true,
@@ -28,24 +39,37 @@ export function SimpleCarousel({ projects, className }: Props) {
 					delay: 5000,
 				}),
 			]}
+			setApi={setApi}
 		>
-			<CarouselContent className="-ml-4">
+			<CarouselContent className="sm:-ml-2 md:-ml-4 -ml-1 w-full">
 				{projects.map((project) => (
-					<CarouselItem
-						className="pl-4 sm:basis-1/2"
+					<CarouselItemWrapper
+						className="w-full pl-1 sm:basis-1/2 sm:pl-4 md:pl-4"
+						// TODO: remove Math.random() when done testing
 						key={project.id + Math.random()}
 					>
 						<div className="p-1">
-							<ProjectsCarouselItem
-								className="border border-red-400"
-								project={project}
-							/>
+							<ProjectsCarouselItem project={project} />
 						</div>
-					</CarouselItem>
+					</CarouselItemWrapper>
 				))}
 			</CarouselContent>
-			<CarouselPrevious />
-			<CarouselNext />
+			<div className="flex items-center justify-center gap-4">
+				<Button
+					className="cursor-pointer"
+					onClick={() => api?.scrollPrev()}
+					variant="ghost"
+				>
+					<MoveLeft /> Prev
+				</Button>
+				<Button
+					className="cursor-pointer"
+					onClick={() => api?.scrollNext()}
+					variant="ghost"
+				>
+					Next <MoveRight />{" "}
+				</Button>
+			</div>
 		</Carousel>
 	);
 }
