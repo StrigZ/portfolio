3bcb76d16fda3c49c0cef946699fe927c806d180
StrigZ <mirgasan200@gmail.com>
2025-12-19 18:25:03 +0300

    Remove theme button

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,16 +1,13 @@
 import { getProjects } from "~/collections/projects/fetchers";
 import DesktopPage from "./_components/Desktop/DesktopPage";
 import Projects from "./_components/Projects";
-import { ThemeButton } from "./_components/ThemeButton";
 
 export default async function page() {
 	const projects = await getProjects();
 
 	return (
 		<>
-			<header className="fixed top-0 left-0">
-				<ThemeButton />
-			</header>
+			<header className="fixed top-0 left-0"></header>
 			<main className="relative mx-auto flex h-full max-w-5xl flex-col items-center justify-center">
 				<DesktopPage
 					className="hidden md:block"
