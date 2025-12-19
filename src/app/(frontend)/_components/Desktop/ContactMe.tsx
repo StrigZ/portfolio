30559f6d29ea781be0ae95a911a21b791421f53d
StrigZ <mirgasan200@gmail.com>
2025-12-19 18:25:13 +0300

    Populate contacts slide

--- src/app/(frontend)/_components/Desktop/ContactMe.tsx
+++ src/app/(frontend)/_components/Desktop/ContactMe.tsx
@@ -1,21 +1,37 @@
 import { buttonVariants } from "../ui/button";
 
-type Props = {};
-export default function ContactMe({}: Props) {
+export default function ContactMe() {
 	return (
 		<div className="flex h-full flex-col items-center justify-center py-4">
 			<h1 className="bold mb-auto font-mono text-5xl tracking-wider">
 				CONTACT ME
 			</h1>
-			<a
-				className={buttonVariants({
-					variant: "link",
-					className: "mb-auto text-xl",
-				})}
-				href="mailto:i@strigz.dev"
-			>
-				i@strigz.dev
-			</a>
+			<ul className="mb-auto flex flex-col gap-2 text-xl">
+				<li>
+					<a
+						className={buttonVariants({
+							variant: "link",
+						})}
+						href="mailto:i@strigz.dev"
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						i@strigz.dev
+					</a>
+				</li>
+				<li>
+					<a
+						className={buttonVariants({
+							variant: "link",
+						})}
+						href="https://github.com/StrigZ"
+						rel="noreferrer noopener"
+						target="_blank"
+					>
+						GitHub
+					</a>
+				</li>
+			</ul>
 		</div>
 	);
 }
