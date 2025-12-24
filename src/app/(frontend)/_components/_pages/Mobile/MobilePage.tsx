1f01cf8a76d5a21a47b573f882f734cb34a4f156
StrigZ <mirgasan200@gmail.com>
2025-12-24 10:38:18 +0300

    Make mobile page snap

--- src/app/(frontend)/_components/_pages/Mobile/MobilePage.tsx
+++ src/app/(frontend)/_components/_pages/Mobile/MobilePage.tsx
@@ -10,14 +10,19 @@ export default function MobilePage({ className }: Props) {
 	const { headings } = useContentProvider();
 
 	return (
-		<div className={cn("flex flex-col justify-between gap-8 pt-4", className)}>
-			<div className="flex min-h-screen flex-col items-center justify-center gap-4">
-				<h2 className="text-center font-mono text-4xl tracking-wider sm:text-5xl">
+		<div
+			className={cn(
+				"flex snap-y snap-mandatory flex-col justify-between gap-8 overflow-auto pt-4",
+				className,
+			)}
+		>
+			<div className="flex min-h-screen snap-center flex-col items-center justify-center py-4">
+				<h2 className="mb-auto text-center font-mono text-4xl tracking-wider sm:text-5xl">
 					{headings?.projects.toUpperCase()}
 				</h2>
-				<SimpleCarousel />
+				<SimpleCarousel className="mb-auto" />
 			</div>
-			<ContactMe className="min-h-screen" />
+			<ContactMe className="min-h-screen snap-center" />
 		</div>
 	);
 }
