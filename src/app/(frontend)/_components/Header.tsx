e8ace05d7eac1df730cf720b6d73df956598bb53
StrigZ <mirgasan200@gmail.com>
2025-12-24 14:47:33 +0300

    Reduce header height on desktop

--- src/app/(frontend)/_components/Header.tsx
+++ src/app/(frontend)/_components/Header.tsx
@@ -7,7 +7,7 @@ export default async function Header({ locale }: Props) {
 	const { localization } = await payloadConfig;
 
 	return (
-		<header className="absolute inset-x-0 top-0 z-50 flex justify-between gap-4 p-2 md:fixed md:inset-x-auto md:inset-y-0 md:left-0 md:flex-col md:justify-start">
+		<header className="absolute inset-x-0 top-0 z-50 flex justify-between gap-4 p-2 md:fixed md:inset-x-auto md:left-0 md:flex-col md:justify-start">
 			<ThemeButton />
 			{localization && (
 				<LocaleSwitcher
